7f645af7368496da4003e31f2b02c996f52b7e27
VisShon <vishnu20414@iiitd.ac.in>
2023-04-29 22:20:38 +0530

    minor routing tweek

--- middleware.js
+++ middleware.js
@@ -4,22 +4,7 @@ import {decodeJwt, jwtVerify} from 'jose'
 export async function middleware(req) {
     const jwt = req.cookies.get("token");
     const {pathname} = req.nextUrl;
-    const{search} = req.nextUrl
-
-    //Profile link redirects
-    if (pathname.startsWith("/profile")||pathname.startsWith("/dashboard")||pathname.startsWith("/host")) {
-        if (jwt === undefined) {
-            req.nextUrl.pathname = "/login";
-            return NextResponse.redirect(req.nextUrl);
-        }
-
-        try {
-            await verify(jwt.value, process.env.JWT_KEY);
-        } catch (error) {
-            req.nextUrl.pathname = "/login";
-            return NextResponse.redirect(req.nextUrl);
-        }
-    }
+    const {search} = req.nextUrl
 
     if(pathname==='/profile'&&!search){
         if (jwt !== undefined) {
@@ -31,39 +16,57 @@ export async function middleware(req) {
         }
     }
 
+
+    //Profile link redirects
+    if (pathname.startsWith("/profile")||pathname.startsWith("/host")) {
+        if (jwt === undefined) {
+            req.nextUrl.pathname = "/login";
+            return NextResponse.redirect(req.nextUrl);
+        }
+        try {
+            await verify(jwt.value, process.env.JWT_KEY);
+            return NextResponse.next()
+        } catch (error) {
+            req.nextUrl.pathname = "/login";
+            return NextResponse.redirect(req.nextUrl);
+        }
+    }
+
     //Dashboard redirects
     if (pathname.startsWith("/dashboard")) {
-        const user = await decode(jwt.value)
+        if (jwt === undefined) {
+            req.nextUrl.pathname = "/login";
+            return NextResponse.redirect(req.nextUrl);
+        }
+        try {
+            await verify(jwt.value, process.env.JWT_KEY);
+            const user = await decode(jwt.value)
+            const regex = /\/dashboard\/([\w-]+)\/(host|attend)/;
+            const match = regex.exec(pathname);
+            const eventId = match[1];
 
-        const regex = /\/dashboard\/([\w-]+)\/(host|attend)/;
-        const match = regex.exec(pathname);
-        const eventId = match[1];
+            const organisedEvents = user.OrganisedEvent.filter(item=>item.id==eventId)
+            const attendedEvents = user.AttendedEvents.filter(item=>item.id==eventId)
 
-        const organisedEvents = user.OrganisedEvent.filter(item=>item.id==eventId)
-        const attendedEvents = user.AttendedEvents.filter(item=>item.id==eventId)
-        
-        console.log(attendedEvents)
+            console.log(organisedEvents)
+            console.log(attendedEvents)
 
-        if(organisedEvents.length!=0&&pathname.endsWith('/host')){
+            if(organisedEvents.length==0&&pathname.endsWith('/host')){
+                req.nextUrl.pathname = `/event/${eventId}`
+                return NextResponse.redirect(req.nextUrl)
+            }
+
+            if(attendedEvents.length==0&&pathname.endsWith('/attend')){
+                req.nextUrl.pathname = `/event/${eventId}`
+                return NextResponse.redirect(req.nextUrl)
+            }
+
             return NextResponse.next()
+            
+        } catch (error) {
+            req.nextUrl.pathname = "/login";
+            return NextResponse.redirect(req.nextUrl);
         }
-
-        if(attendedEvents.length!=0&&pathname.endsWith('/attend')){
-            return NextResponse.next()
-        }
-
-        if(attendedEvents.length!=0&&pathname.endsWith('/host')){
-            req.nextUrl.pathname = `/dashboard/${eventId}/attend`
-            return NextResponse.redirect(req.nextUrl)
-        }
-
-        if(organisedEvents.length!=0&&pathname.endsWith('/attend')){
-            req.nextUrl.pathname = `/dashboard/${eventId}/host`
-            return NextResponse.redirect(req.nextUrl)
-        }
-
-        req.nextUrl.pathname = `/event/${eventId}`
-        return NextResponse.redirect(req.nextUrl)
     }
 
 
@@ -93,11 +96,9 @@ export async function middleware(req) {
 
     //SignUp and Login link redirects
     if (pathname.startsWith("/login")||pathname.startsWith("/signup")) {
-        if (!jwt === undefined) {
-            const user = await decode(jwt.value)
-            req.nextUrl.pathname = '/profile';
-            req.nextUrl.search = `?id=${user.id}`;
-            return NextResponse.redirect(req.nextUrl);
+        if (jwt == undefined) {
+            req.nextUrl.pathname = pathname;
+            return NextResponse.next();
         }
         try {
             await verify(jwt.value, process.env.JWT_KEY);
